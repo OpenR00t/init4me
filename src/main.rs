39f071e4004c8892a3d3e1c1129ae0e4a1554112
OpenR00T <rpclaird@gmail.com>
2022-04-30 17:25:58 -0400

    works now

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use::clap::Parser;
-use::std::process::Command;
+use ::clap::Parser;
+use ::std::process::Command;
+use std::fs;
+use::colored::*;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -11,10 +13,10 @@ struct Args {
 
     /// Name of project
     #[clap(short, long)]
-    name: String,  
+    name: String,
 }
 
-pub fn initRust(proj_name:String) {
+pub fn init_rust(proj_name: String) {
     let mut rust_init = Command::new("cargo");
     rust_init.arg("new");
     rust_init.arg(proj_name);
@@ -22,74 +24,50 @@ pub fn initRust(proj_name:String) {
     //let rust_init_out = rust_init.output();
     //println!("To implement Rust {}", proj_name);
 }
-pub fn initCplus(proj_name:String) {
-    //initializes new directory from name arg
-    let mut init_dir = Command::new("mkdir");
-    init_dir.arg(&proj_name);
-    init_dir.status().expect("process failed to execute");
-    //change into project directory
-    let mut chng_proj = Command::new("cd");
-    chng_proj.arg(proj_name);
-    chng_proj.status().expect("process failed to execute");
-    //makes initialization directory
-    let mut new_subdir = Command::new("mkdir");
-    new_subdir.arg("bin"); //can't parametrically declare dir?
-    new_subdir.arg("src");
-    new_subdir.arg("lib");
-    new_subdir.arg("include");
-    new_subdir.status().expect("process failed to execute");
-    //change to src directory
-    let mut create = Command::new("touch");
-    create.arg("/src/main.cpp");
-    create.arg("/include/main.h");
-    create.status().expect("process failed to execute");
-
-    
+pub fn init_cplus(proj_name: String) -> std::io::Result<()> { 
+    //TODO: maybe make the paths a vector and have it create by iteration?
+    let src_path = format!("./{}/src/{}.cpp", proj_name, proj_name);
+    let bin_path = format!("./{}/bin", proj_name);
+    let include_path = format!("./{}/include/{}.h", proj_name, proj_name);
+    let lib_path = format!("./{}/lib", proj_name);
+    fs::create_dir_all(src_path).unwrap();
+    fs::create_dir_all(bin_path).unwrap();
+    fs::create_dir_all(include_path).unwrap();
+    fs::create_dir_all(lib_path).unwrap();
+    //println!("{}: {} was successfuly intialized", "Created".green().bold(), proj_name.bold());
+    Ok(println!("{}: {} was successfuly intialized as C++", "Created".green().bold(), proj_name.bold()))
 }
-pub fn initPython(proj_name:String) {
-    let mut init_dir = Command::new("mkdir");
-    init_dir.arg(&proj_name);
-    init_dir.status().expect("process failed to execute");
-    let mut chng_proj = Command::new("cd");
-    chng_proj.arg(&proj_name);
-    //initialize filename with a scuffed workaround
-    let mut owned_string:String = proj_name.to_owned();
-    let file_ext: &str = ".py";
-    owned_string.push_str(file_ext);
-    //create the file in project directory
-    let mut create = Command::new("touch");
-    create.arg(owned_string);
-    create.status().expect("process failed to execute");
-    
+pub fn init_py(proj_name: String) -> std::io::Result<()> { 
+    fs::create_dir_all(format!("./{}/{}.py", proj_name, proj_name)).unwrap();
+    println!("{}: {} was successfuly intialized", "Created".green().bold(), proj_name.bold());
+    Ok(())
 }
-pub fn initJavaScript(proj_name:String) {
+pub fn init_js(proj_name: String) { 
     println!("To implement JavaScript {}", proj_name);
-    //plan to add a decision tree
-    //node, react, typescript, etc...
+    //TODO: implement javascript initialization with node, react, typescript modifiers
 }
-pub fn initCsharp(proj_name:String) {
+pub fn init_csharp(proj_name: String) { //TODO: implement c# initialization
     println!("To implement C# {}", proj_name);
+    //TODO: implement c# initialization
 }
-pub fn invalidLang(proj_lang:String ,proj_name:String) {
-    println!("{} is not yet implementd. Cannot initialize {}", proj_lang, proj_name);
+pub fn invalid_lang(proj_lang: &str, proj_name: String) { 
+    println!(
+        "{} is not yet implemented. Cannot initialize {}",
+        proj_lang, proj_name
+    );
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args = Args::parse();
-    
-    //How the hell do I do this another way?
-    let rust_lang = String::from("rust");
-    let cplus_lang = String::from("c++");
-    let python_lang = String::from("python");
-    let javascript_lang = String::from("javascript");
-    let csharp_lang = String::from("c#");
+    let lang_name: &str = &args.lang.to_lowercase();
 
-    match args.lang { //every language keyword just uses cargo??
-        rust_lang => initRust(args.name.to_lowercase()),
-        cplus_lang => initCplus(args.name.to_lowercase()),
-        python_lang => initPython(args.name.to_lowercase()),
-        javascript_lang => initJavaScript(args.name.to_lowercase()),
-        csharp_lang => initCsharp(args.name.to_lowercase()),
-        _ => invalidLang(args.lang, args.name.to_lowercase()),
+    match lang_name {
+        "rust" => init_rust(args.name),
+        "c++" => init_cplus(args.name)?,
+        "python" => init_py(args.name)?,
+        "javascript" => init_js(args.name),
+        "c#" => init_csharp(args.name),
+        _ => invalid_lang(lang_name, args.name),
     }
+    Ok(())
 }
